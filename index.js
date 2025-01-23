1e276a31a8238608644c68fca35031872510c7df
Gautamchandar <gautamnogai@gmail.com>
2025-01-23 19:49:54 +0530

    Changed Navbar and Footer
    I have changed navbar with using gsap and updated footer

---
+++ index.js
@@ -0,0 +1,37 @@
+const nav = document.querySelector(".nav"),
+  searchIcon = document.querySelector("#searchIcon"),
+  navOpenBtn = document.querySelector(".navOpenBtn"),
+  navCloseBtn = document.querySelector(".navCloseBtn");
+
+searchIcon.addEventListener("click", () => {
+  nav.classList.toggle("openSearch");
+  nav.classList.remove("openNav");
+  if (nav.classList.contains("openSearch")) {
+    return searchIcon.classList.replace("uil-search", "uil-times");
+  }
+  searchIcon.classList.replace("uil-times", "uil-search");
+});
+
+navOpenBtn.addEventListener("click", () => {
+  nav.classList.add("openNav");
+  nav.classList.remove("openSearch");
+  searchIcon.classList.replace("uil-times", "uil-search");
+});
+navCloseBtn.addEventListener("click", () => {
+  nav.classList.remove("openNav");
+});
+
+const tl = gsap.timeline()
+tl.from(".logo", {
+  y: -20,
+  opacity:0,
+})
+tl.from(".nav-links li", {
+  y: -20,
+  opacity:0,
+  stagger:1
+})
+tl.from(".uil", {
+  opacity:0,
+  stagger:1
+})
